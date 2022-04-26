4841569ff1087f130777a3971de65f888d6e1c56
jreydc <jreydc@gmail.com>
2022-04-26 19:03:37 +0800

    Player Jump Animation Implementation

--- DemoGameProject/Assets/Scripts/Player.cs
+++ DemoGameProject/Assets/Scripts/Player.cs
@@ -11,6 +11,12 @@ public class Player : MonoBehaviour
     [SerializeField]private float speed;// speed multiplicator for the Player's velocity
     #endregion
 
+    #region Player Ground Checking Declarations
+    [SerializeField]private bool isGrounded; //Linecast variable & indicator returning value if the Player is in the ground
+    [SerializeField]private float jumpForce; //jump force multiplicator for the Player's jump height
+    [SerializeField]private Transform groundCheck;//Endpoint location indicator for the Linecast
+    [SerializeField]private LayerMask playerMask;//To identify the player 
+    #endregion
    
     // Start is called before the first frame update
     void Start()
@@ -18,19 +24,24 @@ public class Player : MonoBehaviour
         rb2d = GetComponent<Rigidbody2D>();//getting the Rigidbody component of the Player gameobject
         anime2r = GetComponent<Animator>();//getting the Animator component of the Player gameobject
         scaleRight = true;
-
+        isGrounded = false;
     }
 
     // FixedUpdate is called in a fixed cycle
     void FixedUpdate()
     {
-  
+        isGrounded = Physics2D.Linecast(rb2d.transform.position, groundCheck.position, playerMask);//Linecast
+        anime2r.SetBool("jumped", !isGrounded);
+        
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            JumpMovement();
+        }
+
         float hInput = Input.GetAxis("Horizontal"); //always checks whether we pressed Horizontal buttons
 
         HandleMovements(hInput);// called method for PlayerMovements
-        Scaling(hInput);//called method for Scaling (facing left-right)
-
-    
+        Scaling(hInput);//called method for Scaling (facing left-right)   
     }
 
     private void HandleMovements(float h)
@@ -40,7 +51,11 @@ public class Player : MonoBehaviour
         anime2r.SetFloat("move", Mathf.Abs(h)); //setting the float (move) parameters in Animator
     }
 
-    
+        private void JumpMovement()
+    {
+        rb2d.velocity = Vector2.up * jumpForce;//computation of the height of the jump using velocity
+    }
+
     private void Scaling(float h)
     {
         if ((h > 0 && !scaleRight) || (h < 0 && scaleRight))
